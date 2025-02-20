71bcb312b18e5a1689ecd16237f9743bb0ddec7b
ShivShakti7445 <141945568+ShivShakti7445@users.noreply.github.com>
2025-02-20 23:48:42 +0530

    chnages

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,26 +1,26 @@
-// import mongoose from "mongoose";
-
-// // Connect to the MongoDB database
-// const connectDB = async () => {
-
-//     mongoose.connection.on('connected', () => console.log('Database Connected'))
-//     await mongoose.connect(`${process.env.MONGODB_URI}/lms-copy`)
-// }
-
-// export default connectDB
-
 import mongoose from "mongoose";
 
 // Connect to the MongoDB database
 const connectDB = async () => {
-    try {
-        mongoose.connection.on('connected', () => console.log('Database Connected'));
-        await mongoose.connect(`${process.env.MONGODB_URI}/lms-copy`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-    } catch (error) {
-        console.error('MongoDB connection error:', error);
-        process.exit(1);
-    }
+
+    mongoose.connection.on('connected', () => console.log('Database Connected'))
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms-copy`)
 }
+
+export default connectDB
+
+// import mongoose from "mongoose";
+
+// // Connect to the MongoDB database
+// const connectDB = async () => {
+//     try {
+//         mongoose.connection.on('connected', () => console.log('Database Connected'));
+//         await mongoose.connect(`${process.env.MONGODB_URI}/lms-copy`, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true
+//         });
+//     } catch (error) {
+//         console.error('MongoDB connection error:', error);
+//         process.exit(1);
+//     }
+// }
