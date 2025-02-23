c4adf4accfae5260ca78498a699e5350c7ab038c
ShivShakti7445 <141945568+ShivShakti7445@users.noreply.github.com>
2025-02-23 15:36:05 +0530

    stripe

--- server/server.js
+++ server/server.js
@@ -1,29 +1,35 @@
+
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
-import { clerkWebhooks } from './controllers/webhooks.js'
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js'
 import educatorRouter from './routes/educatorRoutes.js'
 import { clerkMiddleware } from '@clerk/express'
+import courseRouter from './routes/courseRoute.js'
+import userRouter from './routes/userRoutes.js'
 
 // Initialize Express
 const app = express()
-app.use(clerkMiddleware())
 
 // Connect to database
 await connectDB()
-// await connectCloudinary()
+// await cloudinary()
 
 // Middlewares
 app.use(cors())
+app.use(clerkMiddleware())
 
 //route
 app.get('/', (req, res) => res.send("API Working"))
 app.use('/api/educator', express.json(), educatorRouter)
-app.post('/clerk', express.json() , clerkWebhooks)
+app.post('/clerk', express.json() , clerkWebhooks) 
+app.use('/api/course', express.json(), courseRouter)
+app.use('/api/user', express.json(), userRouter)
+app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
+
 // Port
 const PORT = process.env.PORT || 5000
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
