a39618068b47278baa9b8ced9f0d2fb35edd252c
ShivShakti7445 <141945568+ShivShakti7445@users.noreply.github.com>
2025-03-11 23:16:09 +0530

    added purchase model

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,5 +1,6 @@
 import { clerkClient } from '@clerk/express'
 import Course from '../models/Course.js'
+import {Purchase} from "../models/Purchase.js"
 import cloudinary from '../configs/cloudinary.js'
 import User from '../models/User.js'
 
